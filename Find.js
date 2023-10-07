0fb9130dfead72facb1940121cb99a618e8f5ac9
1122developers <userEmail>
2023-10-07 11:10:57 +0500

    ITMate

--- Find.js
+++ Find.js
@@ -1,7 +1,13 @@
-//Find Array Method
-let userName = ["Zeeshan", "Adnan", "Asif", "Sabri"];
-var result = userName.find((i) => {
-    return i == "Asif";
+// // Find Array Method
+// let userName:string [] = ["Zeeshan","Adnan","Asif","Sabri"]
+// let result:string = userName.find((i)=>{
+//     return i== "Asif"
+// })
+// console.log(result);
+// Fine Array of Object
+let userName1 = [{ Name: "Adnan", age: 10, }, { Name: "Zeeshan Shafi", age: 50 }];
+let response = userName1.find((i) => {
+    return i.Name == "Adnan";
 });
-console.log(result);
+console.log(response);
 export {};
--- Find.ts
+++ Find.ts
@@ -1,10 +1,18 @@
-//Find Array Method
+// // Find Array Method
 
-let userName:string [] = ["Zeeshan","Adnan","Asif","Sabri"]
-let result:string = userName.find((i)=>{
-    return i== "Asif"
+// let userName:string [] = ["Zeeshan","Adnan","Asif","Sabri"]
+// let result:string = userName.find((i)=>{
+//     return i== "Asif"
+// })
+// console.log(result);
+
+// Fine Array of Object
+
+let userName1 = [{Name:"Adnan",age:10,},{Name:"Zeeshan Shafi",age:50}]
+let response= userName1.find((i)=>{
+    return i.Name== "Adnan"
 })
-console.log(result);
+console.log(response);
 
 
 
